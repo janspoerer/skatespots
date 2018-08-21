4dcc10f96dc8c0c91d6eea98fed2ddf88845440d
Jan Spoerer <jan.spoerer@whu.edu>
2018-08-21 14:41:26 +0200

    Added SpotsController

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  belongs_to :city
+  belongs_to :city, optional: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
