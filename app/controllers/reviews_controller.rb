04ecec6c200dd9f8f4958ec350b27137929aa0d7
malcolmtorino <malcolmbyu@gmail.com>
2018-08-21 11:49:53 +0200

    Added review to spots ++BUT must uncomment and c&p code to Spots Controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,17 +1,26 @@
 class ReviewsController < ApplicationController
 
-def index
-#   @location = Location.find(:all)
-  @reviews = Review.all
-end
+  def create
+    @spot = Spot.find(params[:spot_id])
+    @review = Review.new(review_params)
+    @review.spot = @spot
+    if @review.save
+      redirect_to spot_path(@spot)
+    else
+      render 'spots/show'
+    end
+  end
 
-def show
-  @review = Review.find(params[:id])
-end
+# REMEMBER TO ADD TO THE POSTS CONTROLLER
+#   def show
+#     @spot = Spot.find(params[:id])
+#     @review = Review.new
+#   end
 
-def create
-  @review = Review.new(:)
-end
+  private
 
+  def review_params
+    params.require(:review).permit(:content)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :reviews, only: [ :index, :show ]
+  resources :spots, only: [ :index, :show ] do
+    resources :reviews, only: :create
+  end
 end
