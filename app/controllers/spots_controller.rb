b5d0d086072821cbf4c40a5539fef133364fb773
Jan Czizikow <jan.czizikow@gmail.com>
2018-08-22 19:06:23 +0200

    Hp

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,6 +1,8 @@
+# froze_string_literal: true
+
 class SpotsController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i(index show)
-  before_action :set_spot, only: %i(show edit update destroy)
+  skip_before_action :authenticate_user!, only: %i[index show]
+  before_action :set_spot, only: %i[show edit update destroy]
 
   include Pundit
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
@@ -8,17 +10,17 @@ class SpotsController < ApplicationController
 
   def index
     if params[:query].present?
-      @spots = policy_scope(Spot).search_by_title_and_address(params[:query])
+      @spots = policy_scope(Spot).search_by_city_and_name(params[:query])
     else
       @spots = policy_scope(Spot)
       authorize @spots
     end
-    #@markers = Spot.where.not(latitude: nil, longitude: nil).map do |spot|
+    # @markers = Spot.where.not(latitude: nil, longitude: nil).map do |spot|
     #  {
     #    lat: spot.latitude,
     #    lng: spot.longitude
     #  }
-    #end
+    # end
   end
 
   def show
@@ -67,9 +69,21 @@ class SpotsController < ApplicationController
     authorize @spot
   end
 
+  # rubocop:disable Metrics/MethodLength
   def spot_params
-    params[:spot].permit(:title, :price, :description, :capacity, :is_available, :has_captain, :license_plate, :photo, :address)
+    params[:spot].permit(
+      :title,
+      :price,
+      :description,
+      :capacity,
+      :is_available,
+      :has_captain,
+      :license_plate,
+      :photo,
+      :address
+    )
   end
+  # rubocop:enable Metrics/MethodLength
 
   def skip_pundit?
     false
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,30 +1,13 @@
 import "bootstrap";
+import "lazysizes/lazysizes";
+import "lazysizes/plugins/blur-up/ls.blur-up";
 
-import { cityAutocomplete } from '../components/algolia_search.js';
+import { cityAutocomplete } from '../components/nav_search';
+import showHideNavbar from '../components/navbar';
+import rater from '../components/rating';
 
 document.addEventListener('DOMContentLoaded', () => {
+  showHideNavbar();
   cityAutocomplete();
+  rater();
 });
-
-var myRating = raterJs( {
-    // shows a rating tooltip
-    showToolTip: true,
-    // the number of stars
-    max: 5,
-    // star size
-    starSize: 30,
-    // text to show when disabled.
-    disableText: 'Thank you for your vote!',
-    // Text to show when hover over stars.
-    ratingText: '{rating}/{maxRating}',
-    // displayed while user is rating but done not called yet.
-    isBusyText: null,
-    // is readonly?
-    readOnly: false,
-
-    element:document.querySelector("#rater"),
-    rateCallback:function rateCallback(rating, done) {
-      this.setRating();
-      done();
-    }
-  });
