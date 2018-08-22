e6ed36add2f34ba1305207789f833e2e5e894aad
Hien Nguyen <mhien.ng@gmail.com>
2018-08-22 10:13:39 +0200

    added pages controller and created placeholder view for homepage and listing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'spots#index'
+  root to: 'pages#home'
 
   resources :spots
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'dashboard', to: 'dashboard#index'
   resources :profiles, only: [:update, :show]
