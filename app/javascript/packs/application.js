04bfa38db56b09f97a57a3d9d184c42be6b8b067
Jinyong Kim <o2.3ys.v.er@gmail.com>
2018-08-27 23:56:47 +0200

    add navbar color for current page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,9 +4,12 @@ import "lazysizes/plugins/blur-up/ls.blur-up";
 
 import { cityAutocomplete } from '../components/nav_search';
 import showHideNavbar from '../components/navbar';
+import changeTabs from '../components/navbar';
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', (event) => {
   showHideNavbar();
   cityAutocomplete();
+  changeTabs()
 });
+
 
